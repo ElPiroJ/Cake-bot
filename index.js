a6747083d60843d38065057504eed48f3aa10977
if-Johnny <77713194+if-Johnny@users.noreply.github.com>
2022-05-11 23:00:47 -0400

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const Discord = require('discord.js')
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-
-    ]
-})
-client.on('ready', () => {
-    console.log(`logged in as ${client.user.tag}`)
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS",
+  ]
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi") {
-        message.reply("hello world!")
-    }
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
 })
+
+
+const WelcomeChannelId = "781437633984593960"
+
+client.on("guildMemberAdd", async (member) => {
+  const img = await generateImage(member)
+  member.guild.channels.cache.get(WelcomeChannelId).send({
+    content: `Welcome to the server <@${member.id}> \n Enter the <#827114537059876865> to get roles \n Make sure to read the rules and have fun!`,
+    files: [img]
+  })
+  
+})
+
 client.login(process.env.TOKEN)
